3f72aa2692183781018f910782c74639dc70c3b7
ltsambo <aung@ltsambo.co.kr>
2023-09-06 10:36:35 +0800

    file vendor profile from validation

--- vendor/views.py
+++ vendor/views.py
@@ -6,7 +6,8 @@ from accounts.models import UserProfile
 from .models import Vendor
 from django.contrib import messages, auth
 from accounts.views import check_role_vendor
-
+from .forms import VendorForm
+from accounts.forms import UserProfileForm
 # Create your views here.
 
 
@@ -16,24 +17,24 @@ def vprofile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
     vendor = get_object_or_404(Vendor, user=request.user)
     print('vendor', vendor.user_profile.profile_picture)
-    # if request.method == 'POST':
-    #     profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
-    #     vendor_form = VendorForm(request.POST, request.FILES, instance=vendor)
-    #     if profile_form.is_valid() and vendor_form.is_valid():
-    #         profile_form.save()
-    #         vendor_form.save()
-    #         messages.success(request, 'Settings updated.')
-    #         return redirect('vprofile')
-    #     else:
-    #         print(profile_form.errors)
-    #         print(vendor_form.errors)
-    # else:
-    #     profile_form = UserProfileForm(instance = profile)
-    #     vendor_form = VendorForm(instance=vendor)
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        vendor_form = VendorForm(request.POST, request.FILES, instance=vendor)
+        if profile_form.is_valid() and vendor_form.is_valid():
+            profile_form.save()
+            vendor_form.save()
+            messages.success(request, 'Settings updated.')
+            return redirect('vprofile')
+        else:
+            print(profile_form.errors)
+            print(vendor_form.errors)
+    else:
+        profile_form = UserProfileForm(instance = profile)
+        vendor_form = VendorForm(instance=vendor)
 
     context = {
-        # 'profile_form': profile_form,
-        # 'vendor_form': vendor_form,
+        'profile_form': profile_form,
+        'vendor_form': vendor_form,
         'profile': profile,
         'vendor': vendor,
     }
