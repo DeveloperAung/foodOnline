f2c88d3bfc793dd7fa33f9490de1ef8d0c40ff49
ltsambo <aung@ltsambo.co.kr>
2023-09-07 09:26:48 +0800

    food CRUD operation

--- vendor/views.py
+++ vendor/views.py
@@ -10,7 +10,7 @@ from accounts.models import UserProfile
 from accounts.views import check_role_vendor
 
 from menu.models import FoodItem, Vendor, Category
-from menu.forms import Category, CategoryForm
+from menu.forms import Category, CategoryForm, FoodItemForm
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.template.defaultfilters import slugify
 # Create your views here.
@@ -143,24 +143,24 @@ def delete_category(request, pk=None):
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
 def add_food(request):
-    # if request.method == 'POST':
-    #     form = FoodItemForm(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         foodtitle = form.cleaned_data['food_title']
-    #         food = form.save(commit=False)
-    #         food.vendor = get_vendor(request)
-    #         food.slug = slugify(foodtitle)
-    #         form.save()
-    #         messages.success(request, 'Food Item added successfully!')
-    #         return redirect('fooditems_by_category', food.category.id)
-    #     else:
-    #         print(form.errors)
-    # else:
-    #     form = FoodItemForm()
-    #     # modify this form
-    #     form.fields['category'].queryset = Category.objects.filter(vendor=get_vendor(request))
+    if request.method == 'POST':
+        form = FoodItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            foodtitle = form.cleaned_data['food_title']
+            food = form.save(commit=False)
+            food.vendor = get_vendor(request)
+            food.slug = slugify(foodtitle)
+            form.save()
+            messages.success(request, 'Food Item added successfully!')
+            return redirect('fooditems_by_category', food.category.id)
+        else:
+            print(form.errors)
+    else:
+        form = FoodItemForm()
+        # modify this form
+        form.fields['category'].queryset = Category.objects.filter(vendor=get_vendor(request))
     context = {
-        # 'form': form,
+        'form': form,
     }
     return render(request, 'vendor/add_food.html', context)
 
@@ -169,26 +169,27 @@ def add_food(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
 def edit_food(request, pk=None):
-    # food = get_object_or_404(FoodItem, pk=pk)
-    # if request.method == 'POST':
-    #     form = FoodItemForm(request.POST, request.FILES, instance=food)
-    #     if form.is_valid():
-    #         foodtitle = form.cleaned_data['food_title']
-    #         food = form.save(commit=False)
-    #         food.vendor = get_vendor(request)
-    #         food.slug = slugify(foodtitle)
-    #         form.save()
-    #         messages.success(request, 'Food Item updated successfully!')
-    #         return redirect('fooditems_by_category', food.category.id)
-    #     else:
-    #         print(form.errors)
+    food = get_object_or_404(FoodItem, pk=pk)
+    if request.method == 'POST':
+        form = FoodItemForm(request.POST, request.FILES, instance=food)
+        if form.is_valid():
+            foodtitle = form.cleaned_data['food_title']
+            food = form.save(commit=False)
+            food.vendor = get_vendor(request)
+            food.slug = slugify(foodtitle)
+            form.save()
+            messages.success(request, 'Food Item updated successfully!')
+            return redirect('fooditems_by_category', food.category.id)
+        else:
+            print(form.errors)
 
-    # else:
-    #     form = FoodItemForm(instance=food)
-    #     form.fields['category'].queryset = Category.objects.filter(vendor=get_vendor(request))
+    else:
+        form = FoodItemForm(instance=food)
+        # .queryset is get all the values
+        form.fields['category'].queryset = Category.objects.filter(vendor=get_vendor(request))
     context = {
-        # 'form': form,
-        # 'food': food,
+        'form': form,
+        'food': food,
     }
     return render(request, 'vendor/edit_food.html', context)
 
