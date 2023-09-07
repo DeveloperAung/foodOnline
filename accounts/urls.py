fd34a45bdade98699d5f4053888d7891c17477ac
ltsambo <aung@ltsambo.co.kr>
2023-09-07 15:11:29 +0800

    cart functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
     path('', views.myAccount),
     path('registerUser/', views.registerUser, name='registerUser'),
--- accounts/views.py
+++ accounts/views.py
@@ -12,6 +12,7 @@ from .utils import detectUser, send_verification_email
 from django.contrib.auth.decorators import login_required, user_passes_test
 
 from django.core.exceptions import PermissionDenied
+from django.template.defaultfilters import slugify
 # Create your views here.
 
 # Restrict the vendor from accessing the customer page
@@ -93,7 +94,7 @@ def registerVendor(request):
             vendor = v_form.save(commit=False)
             vendor.user = user
             vendor_name = v_form.cleaned_data['vendor_name']
-            # vendor.vendor_slug = slugify(vendor_name)+'-'+str(user.id)
+            vendor.vendor_slug = slugify(vendor_name)+'-'+str(user.id)
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
